22cafa1c58420076491a9f8c34229e90451f5050
selva murugan <selvamurugan.rathinam@colanonline.com>
2022-07-13 00:14:55 +0530

    set all datas

--- src/Pages/Content.js
+++ src/Pages/Content.js
@@ -1,38 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Content(props) {
+  let { state: data } = props.location;
 
+  return (
+    <>
+      <div className="info">
+        <img src={data.Poster} className="poster" />
+        <div>
+          Movie Nmae : <h1 id="title-name">{data.Title}</h1>
+          <br />
+          Dir: <h2 id="title-name">{data.Director}</h2>
+          <div className="rating">
+            <img src="https://dl.dropbox.com/s/c0olu3aadxcm3p7/star-icon.svg?raw=1" />
+            <h4>{data.imdbRating}</h4>
+          </div>
+          <div className="details">
+          Released: <span>{data.Released}</span>
+            Years of Release : <span>{data.Year}</span>
+            Duration : <span>{data.Runtime}</span>
+          </div>
+          <div className="genre">{data.Genre.split(",").join(",")}</div>
+        </div>
 
-    let { state: data } = props.location;
-
-
-    return (
-        <>
-            <div className="info">
-                <img src={data.Poster} className='poster' />
-                <div>
-                    <h2>${data.Title}</h2>
-                    <div className="rating">
-                        <img src="https://dl.dropbox.com/s/c0olu3aadxcm3p7/star-icon.svg?raw=1" />
-                        <h4>${"data.imdbRating"}</h4>
-                    </div>
-                    <div className="details">
-                        <span>${data.Rated}</span>
-                        <span>${data.Year}</span>
-                        <span>${data.Runtime}</span>
-                    </div>
-                    <div className="genre">
-                        <div></div>
-                    </div>
-                </div>
-
-                <h3>Plot:</h3>
-                <p>${data.Plot}</p>
-                <h3>Cast:</h3>
-                <p>${data.Actors}</p>
-            </div>
-        </>
-    )
+        <h3>Plot:</h3>
+        <p>{data.Plot}</p>
+        <h3>Cast:</h3>
+        <p>{data.Actors}</p>
+      </div>
+    </>
+  );
 }
 
 export default Content;
